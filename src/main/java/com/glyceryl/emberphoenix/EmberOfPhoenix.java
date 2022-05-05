a3d91df43be9fef1919123a4752da16646bc8acf
glyceryl <494192175@qq.com>
2022-05-05 22:30:58 +0800

    Merge remote-tracking branch 'github/master'

--- src/main/java/com/glyceryl/emberphoenix/EmberOfPhoenix.java
+++ src/main/java/com/glyceryl/emberphoenix/EmberOfPhoenix.java
@@ -1,13 +1,17 @@
 package com.glyceryl.emberphoenix;
 
+import com.glyceryl.emberphoenix.common.world.registration.EPBiomesKeys;
 import com.glyceryl.emberphoenix.registry.EPBlocks;
 import com.glyceryl.emberphoenix.registry.EPEntity;
 import com.glyceryl.emberphoenix.registry.EPItems;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import java.util.Locale;
 
 @Mod(EmberOfPhoenix.MOD_ID)
 public class EmberOfPhoenix {
@@ -24,5 +28,10 @@ public class EmberOfPhoenix {
     }
 
     private void setup(final FMLCommonSetupEvent event) {
+        EPBiomesKeys.addBiomes();
+    }
+
+    public static ResourceLocation prefix(String name) {
+        return new ResourceLocation(MOD_ID, name.toLowerCase(Locale.ROOT));
     }
 }
--- src/main/java/com/glyceryl/emberphoenix/registry/EPBase.java
+++ src/main/java/com/glyceryl/emberphoenix/registry/EPBase.java
@@ -3,6 +3,7 @@ package com.glyceryl.emberphoenix.registry;
 import com.glyceryl.emberphoenix.EmberOfPhoenix;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.*;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.registries.*;
@@ -13,6 +14,7 @@ public class EPBase {
 
     public static final DeferredRegister<Item> ITEMS = create(ForgeRegistries.ITEMS);
     public static final DeferredRegister<Block> BLOCKS = create(ForgeRegistries.BLOCKS);
+    public static final DeferredRegister<Biome> BIOMES = create(ForgeRegistries.BIOMES);
     public static final DeferredRegister<EntityType<?>> ENTITY = create(ForgeRegistries.ENTITIES);
     public static final DeferredRegister<BlockEntityType<?>> CONTAINER = create(ForgeRegistries.BLOCK_ENTITIES);
 
@@ -33,7 +35,7 @@ public class EPBase {
     public static final CreativeModeTab EP_TAB = new CreativeModeTab("ep_tab") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(EPItems.EMBERIUM_INGOT.get());
+            return new ItemStack(EPItems.SPARK.get());
         }
     };
 }
