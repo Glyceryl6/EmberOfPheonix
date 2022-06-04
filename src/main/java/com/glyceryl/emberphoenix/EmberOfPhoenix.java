ff433edde41c3e02589c2ae1a95895ca66471fd8
glyceryl <494192175@qq.com>
2022-06-04 18:45:21 +0800

    添加了余烬港盔甲，模型有待处理。

--- src/main/java/com/glyceryl/emberphoenix/EmberOfPhoenix.java
+++ src/main/java/com/glyceryl/emberphoenix/EmberOfPhoenix.java
@@ -2,6 +2,7 @@ package com.glyceryl.emberphoenix;
 
 import com.glyceryl.emberphoenix.common.world.biomes.EPBiomesCreator;
 import com.glyceryl.emberphoenix.event.LivingWalkOnMagma;
+import com.glyceryl.emberphoenix.event.RenderFogEvent;
 import com.glyceryl.emberphoenix.registry.*;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
@@ -9,7 +10,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import java.util.Locale;
@@ -18,24 +19,25 @@ import java.util.Locale;
 public class EmberOfPhoenix {
 
     public static final String MOD_ID = "emberphoenix";
+    public IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
     public EmberOfPhoenix() {
-        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        EPBiomesCreator.init();
         EPItems.register(eventBus);
+        EPEntity.register(eventBus);
         EPBlocks.register(eventBus);
         EPBiomes.register(eventBus);
-        EPEntity.register(eventBus);
         EPSounds.register(eventBus);
         EPFeatures.register(eventBus);
         EPContainers.register(eventBus);
         EPEnchantments.register(eventBus);
-        EPBiomesCreator.init();
-        eventBus.addListener(this::setup);
+        eventBus.addListener(this::setupClient);
+        MinecraftForge.EVENT_BUS.register(new RenderFogEvent());
         MinecraftForge.EVENT_BUS.register(new LivingWalkOnMagma());
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event) {
+    private void setupClient(FMLClientSetupEvent event) {
         ItemBlockRenderTypes.setRenderLayer(EPBlocks.ETERNAL_FIRE.get(), RenderType.cutout());
         ItemBlockRenderTypes.setRenderLayer(EPBlocks.BARREN_GRASS.get(), RenderType.cutout());
         ItemBlockRenderTypes.setRenderLayer(EPBlocks.BARREN_TALL_GRASS.get(), RenderType.cutout());
@@ -45,4 +47,5 @@ public class EmberOfPhoenix {
     public static ResourceLocation prefix(String name) {
         return new ResourceLocation(MOD_ID, name.toLowerCase(Locale.ROOT));
     }
+
 }
