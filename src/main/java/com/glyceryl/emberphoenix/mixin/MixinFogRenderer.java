60ac7727a1656bdc32dd53ba3c869adb4d376835
glyceryl <494192175@qq.com>
2022-06-10 17:21:53 +0800

    修改"MixinFogRenderer"中的判断方式

--- src/main/java/com/glyceryl/emberphoenix/mixin/MixinFogRenderer.java
+++ src/main/java/com/glyceryl/emberphoenix/mixin/MixinFogRenderer.java
@@ -5,7 +5,6 @@ import net.minecraft.client.renderer.FogRenderer;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,13 +17,9 @@ public class MixinFogRenderer {
             method = "setupFog(Lnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/FogRenderer$FogMode;FZF)V", at = @At(
             value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;isSpectator()Z", ordinal = 0), remap = false)
     private static boolean setupFog(Entity entity) {
-        if (entity instanceof Player player) {
-            float f6 = getMagmaStrider(player);
-            if (player.fireImmune() || player.hasEffect(MobEffects.FIRE_RESISTANCE) || f6 > 0.0F) {
-                return true;
-            }
-        }
-        return entity.isSpectator();
+        float f6 = getMagmaStrider((LivingEntity) entity);
+        boolean isFireImmune = entity.fireImmune() || ((LivingEntity)entity).hasEffect(MobEffects.FIRE_RESISTANCE);
+        return entity.isSpectator() || isFireImmune || f6 > 0.0F;
     }
 
     private static int getMagmaStrider(LivingEntity entity) {
