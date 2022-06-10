36f623da34437f74b35d22e818b02725872057a8
glyceryl <494192175@qq.com>
2022-06-10 16:57:54 +0800

    修复带有"岩浆探索者"附魔的靴子在岩浆中上升或下降速度慢的bug

--- src/main/java/com/glyceryl/emberphoenix/event/LivingWalkOnMagma.java
+++ src/main/java/com/glyceryl/emberphoenix/event/LivingWalkOnMagma.java
@@ -1,6 +1,7 @@
 package com.glyceryl.emberphoenix.event;
 
 import com.glyceryl.emberphoenix.registry.EPEnchantments;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
@@ -18,16 +19,25 @@ public class LivingWalkOnMagma {
     public void onLivingTravel(LivingEvent event) {
         LivingEntity living = event.getEntityLiving();
         if (living instanceof Player) {
-            Vec3 vec3 = new Vec3(living.xxa, living.yya, living.zza);
             float f6 = (float)getMagmaStrider(living);
+            boolean flag = living.getFluidHeight(FluidTags.LAVA) > living.getFluidJumpThreshold();
             FluidState fluidstate = living.level.getFluidState(living.blockPosition());
             if (living.isInLava() && living.isAffectedByFluids() && !living.canStandOnFluid(fluidstate) && f6 > 0.0F) {
+                float ySpeed;
+                if (living.isCrouching()) {
+                    ySpeed = -f6 / 2.0F;
+                } else if (living.jumping && flag) {
+                    ySpeed = f6 / 2.0F;
+                } else {
+                    ySpeed = living.yya;
+                }
+                Vec3 vec3 = new Vec3(living.xxa, ySpeed, living.zza);
                 living.clearFire();
                 float f5 = 0.02F;
                 if (f6 > 3.0F) {
                     f6 = 3.0F;
                 }
-                f5 += (living.getSpeed() - f5) * f6 / 3.0F;
+                f5 += (living.getSpeed() - f5) * f6 / 6.0F;
                 f5 *= (float) Objects.requireNonNull(living.getAttribute(ForgeMod.SWIM_SPEED.get())).getValue();
                 living.moveRelative(f5, vec3);
             }
