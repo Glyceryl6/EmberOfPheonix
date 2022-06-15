24f44ee2f33550a96b704c301703ca5f184814f9
glyceryl <494192175@qq.com>
2022-06-16 04:20:24 +0800

    删除冗余的代码

--- src/main/java/com/glyceryl/emberphoenix/event/RenderHorizon.java
+++ src/main/java/com/glyceryl/emberphoenix/event/RenderHorizon.java
@@ -1,10 +1,7 @@
 package com.glyceryl.emberphoenix.event;
 
 import com.glyceryl.emberphoenix.common.enchantments.EPEnchantHelper;
-import com.glyceryl.emberphoenix.registry.EPEnchantments;
 import net.minecraft.tags.FluidTags;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderBlockOverlayEvent;
