ccb2b97ed38b4773c8b220623eed49acc54aa9b6
glyceryl <494192175@qq.com>
2022-06-16 04:16:14 +0800

    对"小爆裂弹"进行部分调整

--- src/main/java/com/glyceryl/emberphoenix/common/entity/projectile/SmallCrack.java
+++ src/main/java/com/glyceryl/emberphoenix/common/entity/projectile/SmallCrack.java
@@ -1,7 +1,6 @@
 package com.glyceryl.emberphoenix.common.entity.projectile;
 
 import com.glyceryl.emberphoenix.registry.EPEntity;
-import com.google.common.collect.Maps;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.core.BlockPos;
@@ -13,10 +12,7 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
-import net.minecraft.world.item.enchantment.ProtectionEnchantment;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseFireBlock;
@@ -27,9 +23,7 @@ import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.network.NetworkHooks;
 
 import java.util.List;
-import java.util.Map;
 
-@SuppressWarnings("all")
 public class SmallCrack extends AbstractHurtingProjectile {
 
     public int time;
@@ -76,21 +70,21 @@ public class SmallCrack extends AbstractHurtingProjectile {
     }
 
     //制造一个假的爆炸效果
-    private void createFireAndParticles(int radius) {
+    public void createFireAndParticles(int radius) {
         LevelRenderer levelRenderer = Minecraft.getInstance().levelRenderer;
         levelRenderer.addParticle(ParticleTypes.EXPLOSION_EMITTER, false, this.getX(), this.getY(), this.getZ(), 1.0D, 0.0D, 0.0D);
         levelRenderer.addParticle(ParticleTypes.EXPLOSION, false, this.getX(), this.getY(), this.getZ(), 1.0D, 0.0D, 0.0D);
         this.level.explode(null, getX(), getY(), getZ(), 0.0F, false, Explosion.BlockInteraction.NONE);
         for(BlockPos blockPos : BlockPos.withinManhattan(this.blockPosition(), radius, radius, radius)) {
             boolean hasSolidBlock = this.level.getBlockState(blockPos.below()).isSolidRender(this.level, blockPos.below());
-            if (this.random.nextInt(8) == 0 && this.level.getBlockState(blockPos).isAir() && hasSolidBlock) {
+            if (this.random.nextInt(6) == 0 && this.level.getBlockState(blockPos).isAir() && hasSolidBlock) {
                 this.level.setBlockAndUpdate(blockPos, BaseFireBlock.getState(this.level, blockPos));
             }
         }
     }
 
     //模仿原版的爆炸，并对特定的实体进行伤害
-    private void hurtParticularEntity(float radius) {
+    public void hurtParticularEntity(float radius) {
         float f2 = radius * 2.0F;
         int k1 = Mth.floor(this.getX() - (double)f2 - 1.0D);
         int l1 = Mth.floor(this.getX() + (double)f2 + 1.0D);
@@ -100,7 +94,6 @@ public class SmallCrack extends AbstractHurtingProjectile {
         int j1 = Mth.floor(this.getZ() + (double)f2 + 1.0D);
         List<Entity> list = this.level.getEntitiesOfClass(Entity.class, new AABB(k1, i2, j2, l1, i1, j1));
         Vec3 vec3 = new Vec3(this.getX(), this.getY(), this.getZ());
-        Map<Player, Vec3> hitPlayers = Maps.newHashMap();
         for (Entity entity : list) {
             double d12 = Math.sqrt(entity.distanceToSqr(vec3)) / (double) f2;
             if (d12 <= 1.0D) {
@@ -113,18 +106,9 @@ public class SmallCrack extends AbstractHurtingProjectile {
                     d7 /= d13;
                     d9 /= d13;
                     double d10 = (1.0D - d12);
-                    double d11 = d10;
-                    if (entity instanceof LivingEntity) {
-                        d11 = ProtectionEnchantment.getExplosionKnockbackAfterDampener((LivingEntity) entity, d10);
-                    }
                     if (!entity.fireImmune()) {
                         entity.hurt(DamageSource.FALL, (float) ((int) ((d10 * d10 + d10) / 2.0D * 7.0D * (double) f2 + 1.0D)));
-                        entity.setDeltaMovement(entity.getDeltaMovement().add(d5 * d11, d7 * d11, d9 * d11));
-                    }
-                    if (entity instanceof Player player) {
-                        if (!player.isSpectator() && (!player.isCreative() || !player.getAbilities().flying)) {
-                            hitPlayers.put(player, new Vec3(d5 * d10, d7 * d10, d9 * d10));
-                        }
+                        entity.setDeltaMovement(entity.getDeltaMovement().add(d5 * d10, d7 * d10, d9 * d10));
                     }
                 }
             }
