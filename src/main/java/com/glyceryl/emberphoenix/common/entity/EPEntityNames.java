fd4910f4f7335f7787beecc7ca37c62d46c796dc
glyceryl <494192175@qq.com>
2022-06-17 14:09:22 +0800

    添加注册名"weeper"和"unfortunate"以备用

--- src/main/java/com/glyceryl/emberphoenix/common/entity/EPEntityNames.java
+++ src/main/java/com/glyceryl/emberphoenix/common/entity/EPEntityNames.java
@@ -4,9 +4,12 @@ import net.minecraft.resources.ResourceLocation;
 
 import static com.glyceryl.emberphoenix.EmberOfPhoenix.prefix;
 
+@SuppressWarnings("unused")
 public class EPEntityNames {
 
+    public static final ResourceLocation WEEPER = prefix("weeper");
     public static final ResourceLocation WILDFIRE = prefix("wildfire");
+    public static final ResourceLocation UNFORTUNATE = prefix("unfortunate");
     public static final ResourceLocation SMALL_CRACK = prefix("small_crack");
     public static final ResourceLocation PALE_FIREBALL = prefix("pale_fireball");
     public static final ResourceLocation ANCIENT_BLAZE = prefix("ancient_blaze");
