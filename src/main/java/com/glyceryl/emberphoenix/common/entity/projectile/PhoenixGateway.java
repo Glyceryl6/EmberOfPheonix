c1c007c57e2364fe301f9c84e2f403309bc9d75a
glyceryl <494192175@qq.com>
2022-06-26 02:14:06 +0800

    正式给传送门实体加上维度传送的功能

--- src/main/java/com/glyceryl/emberphoenix/common/entity/projectile/PhoenixGateway.java
+++ src/main/java/com/glyceryl/emberphoenix/common/entity/projectile/PhoenixGateway.java
@@ -1,14 +1,21 @@
 package com.glyceryl.emberphoenix.common.entity.projectile;
 
+import com.glyceryl.emberphoenix.registry.EPDimensions;
 import com.glyceryl.emberphoenix.registry.EPEntity;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraftforge.common.util.ITeleporter;
+
+import java.util.function.Function;
 
 @SuppressWarnings("unused")
 public class PhoenixGateway extends Entity {
@@ -59,6 +66,17 @@ public class PhoenixGateway extends Entity {
                 this.level.addParticle(ParticleTypes.PORTAL, dx, dy, dz, d0, d1, d2);
             }
         }
+        if (!this.level.isClientSide) {
+            for (Entity entity : this.level.getEntities(this, getBoundingBox())) {
+                if (!entity.isPassenger() && !entity.isVehicle() && entity.canChangeDimensions()) {
+                    if (this.level.getServer().getLevel(EPDimensions.PHOENIX_KEY) == this.level) {
+                        this.teleport(entity, entity.getServer().getLevel(Level.OVERWORLD), entity.getOnPos(), true);
+                    } else if (this.level.getServer().getLevel(Level.OVERWORLD) == this.level) {
+                        this.teleport(entity, entity.getServer().getLevel(EPDimensions.PHOENIX_KEY), entity.getOnPos(), true);
+                    }
+                }
+            }
+        }
     }
 
     @Override
@@ -69,6 +87,21 @@ public class PhoenixGateway extends Entity {
     @Override
     public Packet<?> getAddEntityPacket() {
         return new ClientboundAddEntityPacket(this);
+    }
+
+    public void teleport(Entity entity, ServerLevel destination, BlockPos pos, boolean findTop) {
+        entity.changeDimension(destination, new ITeleporter() {
+            @Override
+            public Entity placeEntity(Entity entity, ServerLevel currentWorld, ServerLevel destWorld, float yaw, Function<Boolean, Entity> repositionEntity) {
+                entity = repositionEntity.apply(false);
+                int y = pos.getY();
+                if (findTop) {
+                    y = destination.getHeight(Heightmap.Types.WORLD_SURFACE_WG, pos.getX(), pos.getZ());
+                }
+                entity.teleportTo(pos.getX(), y, pos.getZ());
+                return entity;
+            }
+        });
     }
 
 }
