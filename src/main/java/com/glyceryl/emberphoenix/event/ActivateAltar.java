b26226b9f164e17dfc772442db4c41d5d45ebea6
glyceryl <494192175@qq.com>
2022-06-25 20:32:41 +0800

    对代码排版进行调整

--- src/main/java/com/glyceryl/emberphoenix/event/ActivateAltar.java
+++ src/main/java/com/glyceryl/emberphoenix/event/ActivateAltar.java
@@ -26,20 +26,21 @@ public class ActivateAltar {
 
     @SubscribeEvent
     public void onUseDiamondOnAltar(PlayerInteractEvent.RightClickBlock event) {
-        int range = 2;
         Level level = event.getWorld();
         Player player = event.getPlayer();
+        Block fire = EPBlocks.ETERNAL_FIRE.get();
+        Block altar = EPBlocks.ETERNAL_FIRE_ALTAR.get();
         InteractionHand hand = player.getUsedItemHand();
         ItemStack itemStack = player.getItemInHand(hand);
         BlockPos blockPos = event.getHitVec().getBlockPos();
         BlockState blockState = level.getBlockState(blockPos);
-        if (itemStack.is(Items.DIAMOND) && blockState.is(EPBlocks.ETERNAL_FIRE_ALTAR.get())) {
+        if (itemStack.is(Items.DIAMOND) && blockState.is(altar)) {
             Direction direction = event.getHitVec().getDirection();
             blockPos = blockPos.relative(direction);
+            boolean isNoOtherEntity = true;
+            boolean hasEnoughSolidBlock = true;
             boolean isUp = direction == Direction.UP;
             boolean isAir = level.getBlockState(blockPos).isAir();
-            boolean onlyOneDiamond = true;
-            boolean hasEnoughSolidBlock = true;
             //检测玩家是否是对着祭坛的顶部使用钻石且该顶部方块为空气
             if (!level.isClientSide && isAir && isUp) {
                 BlockPos centerPos = new BlockPos(Vec3.atCenterOf(blockPos));
@@ -49,21 +50,20 @@ public class ActivateAltar {
                 int gatewayCount = level.getEntitiesOfClass(PhoenixGateway.class, aabb).size();
                 //检测祭坛周围区域是否已存在创建器实体或传送门实体，没有则继续
                 if (creatorCount <= 0 && gatewayCount <= 0) {
-                    List<BlockPos> baseBlockCount = new ArrayList<>(List.of());
                     //检测召唤区域内是否还存在其它的无关方块
-                    for (BlockPos pos : BlockPos.withinManhattan(centerPos, range, 1, range)) {
+                    List<BlockPos> baseBlockCount = new ArrayList<>(List.of());
+                    for (BlockPos pos : BlockPos.withinManhattan(centerPos, 2, 1, 2)) {
                         if (!level.getBlockState(pos).isAir()) {
                             baseBlockCount.add(pos);
                         }
                     }
-                    //检测四个角是否均存在永恒之火
-                    Block fire = EPBlocks.ETERNAL_FIRE.get();
+                    //检测四个角是否均放置了祭坛，并且已经点火（其实只用检查是否有火就行，因为火只存在于祭坛之上，有火必有祭坛）
                     BlockState state1 = level.getBlockState(centerPos.relative(Direction.EAST, 2).relative(Direction.NORTH, 2));
                     BlockState state2 = level.getBlockState(centerPos.relative(Direction.EAST, 2).relative(Direction.SOUTH, 2));
                     BlockState state3 = level.getBlockState(centerPos.relative(Direction.WEST, 2).relative(Direction.NORTH, 2));
                     BlockState state4 = level.getBlockState(centerPos.relative(Direction.WEST, 2).relative(Direction.SOUTH, 2));
                     boolean hasEternalFire = state1.is(fire) && state2.is(fire) && state3.is(fire) && state4.is(fire);
-                    //满足剩余所有条件后，召唤一个传送门创建器
+                    //满足剩余所有条件后，召唤一个传送门创建器实体
                     if (baseBlockCount.size() == 9 && hasEternalFire) {
                         GatewayCreator creator = new GatewayCreator(EPEntity.GATEWAY_CREATOR.get(), level);
                         creator.setPos(Vec3.atCenterOf(blockPos));
@@ -73,11 +73,11 @@ public class ActivateAltar {
                         hasEnoughSolidBlock = false;
                     }
                 } else {
-                    onlyOneDiamond = false;
+                    isNoOtherEntity = false;
                 }
             }
             player.awardStat(Stats.ITEM_USED.get(itemStack.getItem()));
-            if (!player.getAbilities().instabuild && isUp && onlyOneDiamond && hasEnoughSolidBlock) {
+            if (!player.getAbilities().instabuild && isUp && isNoOtherEntity && hasEnoughSolidBlock) {
                 itemStack.shrink(1);
             }
             player.swing(hand);
