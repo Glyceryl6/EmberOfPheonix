5c4df5ed074bdce478c055b300bb22ea9852370c
glyceryl <494192175@qq.com>
2022-06-27 13:22:39 +0800

    给"红石浆果丛"添加强充能和弱充能的特性

--- src/main/java/com/glyceryl/emberphoenix/common/blocks/RedstoneBerryBush.java
+++ src/main/java/com/glyceryl/emberphoenix/common/blocks/RedstoneBerryBush.java
@@ -3,6 +3,8 @@ package com.glyceryl.emberphoenix.common.blocks;
 import com.glyceryl.emberphoenix.registry.EPBlocks;
 import com.glyceryl.emberphoenix.registry.EPItems;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
@@ -12,28 +14,56 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SweetBerryBushBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.phys.BlockHitResult;
+
+import java.util.Random;
 
 @SuppressWarnings("deprecation")
 public class RedstoneBerryBush extends SweetBerryBushBlock {
 
-    public static final IntegerProperty POWER = BlockStateProperties.POWER;
-
     public RedstoneBerryBush(Properties properties) {
         super(properties);
-        this.registerDefaultState(this.stateDefinition.any().setValue(AGE, 0).setValue(POWER, 0));
+        this.registerDefaultState(this.stateDefinition.any().setValue(AGE, 0));
+    }
+
+    @Override
+    public void onPlace(BlockState blockState, Level level, BlockPos blockPos, BlockState p_55727_, boolean p_55728_) {
+        for(Direction direction : Direction.values()) {
+            if (blockState.getValue(AGE) == 3) {
+                level.updateNeighborsAt(blockPos.relative(direction), this);
+            }
+        }
+    }
+
+    @Override
+    public void onRemove(BlockState blockState, Level level, BlockPos blockPos, BlockState p_55709_, boolean p_55710_) {
+        if (!p_55710_) {
+            for(Direction direction : Direction.values()) {
+                if (blockState.getValue(AGE) == 3) {
+                    level.updateNeighborsAt(blockPos.relative(direction), this);
+                }
+            }
+        }
+    }
+
+    @Override
+    public ItemStack getCloneItemStack(BlockGetter p_57256_, BlockPos p_57257_, BlockState p_57258_) {
+        return new ItemStack(EPItems.REDSTONE_BERRIES.get());
     }
 
     @Override
     protected boolean mayPlaceOn(BlockState state, BlockGetter getter, BlockPos pos) {
         return state.is(EPBlocks.SCARLET_DIRT.get());
     }
+
+    @Override
+    public int getSignal(BlockState p_60483_, BlockGetter p_60484_, BlockPos p_60485_, Direction p_60486_) {
+        return 15;
+    }
+
+
 
     @Override
     public boolean isSignalSource(BlockState p_55213_) {
@@ -56,11 +86,6 @@ public class RedstoneBerryBush extends SweetBerryBushBlock {
         } else {
             return super.use(state, level, pos, player, hand, result);
         }
-    }
-
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(AGE).add(POWER);
     }
 
 }
