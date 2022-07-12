8ea39746f8b4de886f1c854b62467423b3175389
glyceryl <494192175@qq.com>
2022-07-12 22:56:31 +0800

    设置在回血状态下的"野火"带有附魔光效。

--- src/main/java/com/glyceryl/emberphoenix/client/renderer/WildfireRenderer.java
+++ src/main/java/com/glyceryl/emberphoenix/client/renderer/WildfireRenderer.java
@@ -5,17 +5,107 @@ import com.glyceryl.emberphoenix.client.EPModelLayers;
 import com.glyceryl.emberphoenix.client.model.WildfireModel;
 import com.glyceryl.emberphoenix.common.entity.boss.WildfireEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Pose;
+
+import java.util.Objects;
 
 public class WildfireRenderer extends MobRenderer<WildfireEntity, WildfireModel<WildfireEntity>> {
 
     private static final ResourceLocation WILDFIRE_LOCATION = EmberOfPhoenix.prefix("textures/entity/wildfire.png");
 
     public WildfireRenderer(EntityRendererProvider.Context context) {
-        super(context, new WildfireModel<>(context.bakeLayer(EPModelLayers.WILDFIRE_LAYER)), 0.5F);
+        super(context, new WildfireModel<>(context.bakeLayer(EPModelLayers.WILDFIRE_LAYER)), 1.0F);
+    }
+
+    @Override
+    public void render(WildfireEntity entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight) {
+        poseStack.pushPose();
+        this.model.attackTime = this.getAttackAnim(entity, partialTicks);
+        boolean shouldSit = entity.isPassenger() && (entity.getVehicle() != null && entity.getVehicle().shouldRiderSit());
+        this.model.riding = shouldSit;
+        this.model.young = entity.isBaby();
+        float f = Mth.rotLerp(partialTicks, entity.yBodyRotO, entity.yBodyRot);
+        float f1 = Mth.rotLerp(partialTicks, entity.yHeadRotO, entity.yHeadRot);
+        float f2 = f1 - f;
+        if (shouldSit && entity.getVehicle() instanceof LivingEntity livingentity) {
+            f = Mth.rotLerp(partialTicks, livingentity.yBodyRotO, livingentity.yBodyRot);
+            f2 = f1 - f;
+            float f3 = Mth.wrapDegrees(f2);
+            if (f3 < -85.0F) {
+                f3 = -85.0F;
+            }
+
+            if (f3 >= 85.0F) {
+                f3 = 85.0F;
+            }
+
+            f = f1 - f3;
+            if (f3 * f3 > 2500.0F) {
+                f += f3 * 0.2F;
+            }
+
+            f2 = f1 - f;
+        }
+
+        float f6 = Mth.lerp(partialTicks, entity.xRotO, entity.getXRot());
+        if (isEntityUpsideDown(entity)) {
+            f6 *= -1.0F;
+            f2 *= -1.0F;
+        }
+
+        if (entity.getPose() == Pose.SLEEPING) {
+            Direction direction = entity.getBedOrientation();
+            if (direction != null) {
+                float f4 = entity.getEyeHeight(Pose.STANDING) - 0.1F;
+                poseStack.translate((float)(-direction.getStepX()) * f4, 0.0D, (float)(-direction.getStepZ()) * f4);
+            }
+        }
+
+        float f7 = this.getBob(entity, partialTicks);
+        this.setupRotations(entity, poseStack, f7, f, partialTicks);
+        poseStack.scale(-1.0F, -1.0F, 1.0F);
+        this.scale(entity, poseStack, partialTicks);
+        poseStack.translate(0.0D, -1.501F, 0.0D);
+        float f8 = 0.0F;
+        float f5 = 0.0F;
+        if (!shouldSit && entity.isAlive()) {
+            f8 = Mth.lerp(partialTicks, entity.animationSpeedOld, entity.animationSpeed);
+            f5 = entity.animationPosition - entity.animationSpeed * (1.0F - partialTicks);
+            if (entity.isBaby()) {
+                f5 *= 3.0F;
+            }
+
+            if (f8 > 1.0F) {
+                f8 = 1.0F;
+            }
+        }
+
+        this.model.prepareMobModel(entity, f5, f8, partialTicks);
+        this.model.setupAnim(entity, f5, f8, f7, f2, f6);
+        Minecraft minecraft = Minecraft.getInstance();
+        boolean flag = this.isBodyVisible(entity);
+        boolean flag1 = !flag && !entity.isInvisibleTo(Objects.requireNonNull(minecraft.player));
+        boolean flag2 = minecraft.shouldEntityAppearGlowing(entity);
+        RenderType rendertype = this.getRenderType(entity, flag, flag1, flag2);
+        if (rendertype != null) {
+            VertexConsumer foilBufferDirect = ItemRenderer.getFoilBufferDirect(buffer, this.model.renderType(this.getTextureLocation(entity)), false, entity.isFoil());
+            int i = getOverlayCoords(entity, this.getWhiteOverlayProgress(entity, partialTicks));
+            this.model.renderToBuffer(poseStack, foilBufferDirect, packedLight, i, 1.0F, 1.0F, 1.0F, flag1 ? 0.15F : 1.0F);
+        }
+
+        poseStack.popPose();
     }
 
     @Override
@@ -29,9 +119,9 @@ public class WildfireRenderer extends MobRenderer<WildfireEntity, WildfireModel<
     }
 
     @Override
-    protected void scale(WildfireEntity entity, PoseStack stack, float partialTickTime) {
+    protected void scale(WildfireEntity entity, PoseStack poseStack, float partialTickTime) {
         float scale = 2.0F;
-        stack.scale(scale, scale, scale);
+        poseStack.scale(scale, scale, scale);
     }
 
 }
