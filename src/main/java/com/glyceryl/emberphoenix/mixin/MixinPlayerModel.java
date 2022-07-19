c1982384dcd9531615b46469bed2d7d0d452d6ce
glyceryl <494192175@qq.com>
2022-07-19 10:34:22 +0800

    修复玩家手持"灰烬手炮"时袖子不转动的问题

--- src/main/java/com/glyceryl/emberphoenix/mixin/MixinPlayerModel.java
+++ src/main/java/com/glyceryl/emberphoenix/mixin/MixinPlayerModel.java
@@ -6,13 +6,19 @@ import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerModel.class)
 public class MixinPlayerModel<T extends LivingEntity> extends HumanoidModel<T> {
+
+    @Shadow @Final public ModelPart rightSleeve;
+
+    @Shadow @Final public ModelPart leftSleeve;
 
     public MixinPlayerModel(ModelPart root) {
         super(root);
@@ -23,10 +29,14 @@ public class MixinPlayerModel<T extends LivingEntity> extends HumanoidModel<T> {
         ItemStack mainHandItem = entity.getMainHandItem();
         ItemStack offHandItem = entity.getOffhandItem();
         if (mainHandItem.is(EPItems.CINDER_CANNON.get())) {
+            this.rightSleeve.xRot = (-(float)Math.PI / 2F) + this.head.xRot;
+            this.rightSleeve.yRot = 0.0F;
             this.rightArm.xRot = (-(float)Math.PI / 2F) + this.head.xRot;
             this.rightArm.yRot = 0.0F;
         }
         if (offHandItem.is(EPItems.CINDER_CANNON.get())) {
+            this.leftSleeve.xRot = (-(float)Math.PI / 2F) + this.head.xRot;
+            this.leftSleeve.yRot = 0.0F;
             this.leftArm.xRot = (-(float)Math.PI / 2F) + this.head.xRot;
             this.leftArm.yRot = 0.0F;
         }
